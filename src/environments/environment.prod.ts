dbb0c564c702dc3216b24380f1598bd164873393
mthesoliver <m.limaoliveira96@gmail.com>
2024-08-05 18:04:56 -0300

    Ajuste redirect

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   authorize_uri: 'http://auth-service:9000/oauth2/authorize?',
   authorize_gateway:'/oauth2/authorization/gateway?',
-  redirect_gateway:'reserva-gateway-production.up.railway.app/oauth2/authorization/gateway?',
+  redirect_gateway:'https://reserva-gateway-production.up.railway.app/oauth2/authorization/gateway?',
   client_id:'reserva-com',
   redirect_uri:'http://127.0.0.1:4200/authorized http://127.0.0.1:8081/login/oauth2/code/gateway http://reserva-gateway:8081/login/oauth2/code/gateway',
   scope:'openid profile',
