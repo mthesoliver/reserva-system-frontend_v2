f60e6599aa95960c537d1e0dd894408fa700b49f
mthesoliver <m.limaoliveira96@gmail.com>
2024-08-07 13:30:11 -0300

    change client

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   authorize_uri: 'https://reserva-auth-service.up.railway.app/oauth2/authorize?',
   authorize_gateway:'/oauth2/authorization/gateway?',
-  redirect_gateway:'https://reserva-gateway-production.up.railway.app/oauth2/authorization/gateway?',
+  redirect_gateway:'https://reserva-gateway-production.up.railway.app/login/oauth2/authorization/gateway?',
   client_id:'reserva-com',
   redirect_uri:'https://reserva-auth-service.up.railway.app/authorized https://reserva-gateway-production.up.railway.app/login/oauth2/code/gateway',
   scope:'openid profile',
