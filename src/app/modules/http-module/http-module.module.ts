aeb62a12840ec408777b4fb2e124292547ccb2ac
mthesoliver <m.limaoliveira96@gmail.com>
2024-08-11 18:44:39 -0300

    Configure the interceptors

--- src/app/modules/http-module/http-module.module.ts
+++ src/app/modules/http-module/http-module.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { AuthInterceptor } from 'src/app/interceptors/auth.interceptor';
 
 
 
@@ -11,7 +12,10 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
   exports:[
     HttpClientModule
   ],
-  providers:[
-  ]
+  providers:[{
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }]
 })
 export class HttpModuleModule { }
