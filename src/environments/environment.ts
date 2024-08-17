17f539d994d04172760ed7651ded1cc8abfa6a92
mthesoliver <m.limaoliveira96@gmail.com>
2024-08-17 13:32:31 -0300

    Change client credentials

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,22 +1,23 @@
 
 export const environment = {
-  production: false,
+  production: true,
   authorize_uri: 'https://reserva-auth-service.up.railway.app/oauth2/authorize?',
-  authorize_gateway:'/oauth2/authorization/gateway?',
-  redirect_gateway:'https://reserva-gateway-production.up.railway.app/oauth2/authorization/gateway?',
-  client_id:'reserva-com',
-  redirect_uri:'https://reserva-auth-service.up.railway.app/authorized https://reserva-gateway-production.up.railway.app/login/oauth2/code/gateway',
-  scope:'openid profile',
-  response_type:'code',
-  response_mode:'form_post',
-  code_challenge_method:'S256',
-  token_url:'https://reserva-auth-service.up.railway.app/oauth2/token',
-  grant_type:'authorization_code',
-  logout_url:'https://reserva-auth-service.up.railway.app/logout',
-  secret_pkce:'secret'
+  authorize_gateway: '/oauth2/authorization/gateway?',
+  redirect_gateway: 'https://reserva-gateway-production.up.railway.app/oauth2/authorization/gateway?',
+  client_id: 'railway',
+  redirect_uri: 'https://reserva-auth-service.up.railway.app/authorized https://reserva-gateway-production.up.railway.app/login/oauth2/code/gateway',
+  scope: 'openid profile',
+  response_type: 'code',
+  response_mode: 'form_post',
+  code_challenge_method: 'S256',
+  token_url: 'https://reserva-auth-service.up.railway.app/oauth2/token',
+  grant_type: 'authorization_code',
+  logout_url: 'https://reserva-auth-service.up.railway.app/logout',
+  secret_pkce: 'railway_123'
 };
 
 
-  // code_challenge:'vEH-FoAkWzg0OUM7O_XXAcWPPfdMTiYzu9tOwRGE9LE',
-  // code_verifier:'e7XppJEBTfMSu5i7xUIzKY8rysnghiLw8kIOViZFwdD',
+
+// code_challenge:'vEH-FoAkWzg0OUM7O_XXAcWPPfdMTiYzu9tOwRGE9LE',
+// code_verifier:'e7XppJEBTfMSu5i7xUIzKY8rysnghiLw8kIOViZFwdD',
 
